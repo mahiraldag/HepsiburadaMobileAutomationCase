b4c3f528f316544ddd1d908f0ba1fef826a2c389
Mahir <mahiraldag@gmail.com>
2022-01-02 21:07:20 +0300

    Update Scenario1.java

--- HepsiburadaAppiumCase/src/main/java/Scenario1.java
+++ HepsiburadaAppiumCase/src/main/java/Scenario1.java
@@ -23,7 +23,7 @@ public class Scenario1 {
     By areaDropdown = By.xpath("//android.widget.ImageView[@content-desc=\"Mahalle seçin\"]");
     By randomArea = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.FrameLayout/androidx.cardview.widget.CardView/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[3]/android.widget.TextView");
     By locationSaveButton = By.id("com.pozitron.hepsiburada:id/button");
-    By locationSavedPopUpClose = By.id("com.pozitron.hepsiburada:id/ivClose"); //"Konumunuz kaydedildi"
+    By locationSavedPopUpClose = By.id("com.pozitron.hepsiburada:id/ivClose"); 
     By categoryTabButton = By.id("com.pozitron.hepsiburada:id/nav_graph_category") ;
     By sizeOzel = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/androidx.cardview.widget.CardView[1]/android.view.ViewGroup/android.widget.TextView");
     By randomCategory = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/androidx.cardview.widget.CardView/android.widget.ImageView");
@@ -45,9 +45,9 @@ public class Scenario1 {
     @Test(dependsOnMethods = "openLocationSection")
     public void enterLocationDetails() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(cityDropdown)).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(randomCity)).click();  //Afyon
+        wait.until(ExpectedConditions.visibilityOfElementLocated(randomCity)).click(); 
         wait.until(ExpectedConditions.visibilityOfElementLocated(districtDropdown)).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(randomDistrict)).click();  // Bolvadin
+        wait.until(ExpectedConditions.visibilityOfElementLocated(randomDistrict)).click();  
         wait.until(ExpectedConditions.visibilityOfElementLocated(areaDropdown)).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(randomArea)).click();
     }
@@ -57,7 +57,7 @@ public class Scenario1 {
         wait.until(ExpectedConditions.visibilityOfElementLocated(locationSaveButton)).click();
     }
 
-    // Tries to click on "X" to both check & close the pop-up
+   
     @Test(dependsOnMethods = "saveLocationDetails")
     public void checkLocationSavedPopup() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(locationSavedPopUpClose));
